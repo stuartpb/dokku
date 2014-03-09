3bab47649abcae70ad311ac5dda9469d0d08b8af
Stuart P. Bentley <stuart@testtrack4.com>
2014-03-08 23:50:36 -0500

    Add --server designation

--- contrib/dokku-client.sh
+++ contrib/dokku-client.sh
@@ -5,29 +5,53 @@
 # To install, add it as an executable script in your local ~/bin directory
 # under the name you want to call it as.
 
-case $1 in
-  --remote)
-    shift
-    REMOTE=$1
-    shift
-    ;;
-  --remote=*)
-    REMOTE=${1#--remote=}
-    shift
-esac
+USERNAME=dokku
 
-REMOTE=${REMOTE:-dokku}
-REMOTE_URL=$(git config --get remote.$REMOTE.url)
+while true; do
+    case $1 in
+      --remote)
+        shift
+        REMOTE=$1
+        shift
+        ;;
+      --remote=*)
+        REMOTE=${1#--remote=}
+        shift
+        ;;
+      --server)
+        shift
+        SERVER="$USERNAME@$1"
+        shift
+        ;;
+      --server=*)
+        SERVER="$USERNAME@${1#--server=}"
+        shift
+        ;;
+      --)
+        shift
+        break
+        ;;
+      *)
+        break
+        ;;
+    esac
+done
 
-if [ $? -ne 0 ]; then
-    echo "Remote \"$REMOTE\" not found"
-    exit 1
+if [ -z $SERVER ]; then
+    REMOTE=${REMOTE:-dokku}
+    REMOTE_URL=$(git config --get remote.$REMOTE.url)
+
+    if [ $? -ne 0 ]; then
+        echo "Remote \"$REMOTE\" not found"
+        exit 1
+    fi
+
+    if [ ${REMOTE_URL%%@*} != $USERNAME ]; then
+        echo "Remote \"$REMOTE\" does not appear to be a Dokku server"
+        exit 1
+    fi
+
+    SERVER=${REMOTE_URL%%:*}
 fi
 
-if [ ${REMOTE_URL%%@*} != "dokku" ]; then
-    echo "Remote \"$REMOTE\" does not appear to be a Dokku server"
-    exit 1
-fi
-
-REMOTE_HOST=${REMOTE_URL%%:*}
-ssh "$REMOTE_HOST" "$@"
+ssh "$SERVER" "$@"
